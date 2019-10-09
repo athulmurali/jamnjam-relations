435924191805c5cbedee710df0e383412afafec4
Athul Muralidharan <athulmurali@gmail.com>
2019-10-08 20:55:29 -0400

    Added docker compose for neo4j

--- server/envValidator.js
+++ server/envValidator.js
@@ -3,7 +3,19 @@ const { str, email, json,port } = envalid
 
 const env = envalid.cleanEnv(process.env, {
     NODE_SERVER_PORT :  port({default : 3000}),
+    NEO4J_PROTOCOL  : str({default: 'bolt'}),
+    NEO4J_HOST: str({default: 'localhost'}),
+    NEO4J_USERNAME : str
+
+
+
     // API_KEY:            str(),
     // ADMIN_EMAIL:        email({ default: 'admin@example.com' }),
     // EMAIL_CONFIG_JSON:  json({ desc: 'Additional email parameters' })
 })
+
+NEO4J_PROTOCOL=bolt
+NEO4J_HOST=localhost
+NEO4J_USERNAME=neo4j
+NEO4J_PASSWORD=neo4j
+NEO4J_PORT=7687
